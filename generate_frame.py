8199ad5b7d110b546575830214c7c0d0b7c5ac23
yhbian <49975503+yhbian@users.noreply.github.com>
2020-07-16 16:25:44 +0800

    Add files via upload

--- generate_frame.py
+++ generate_frame.py
@@ -5,6 +5,7 @@ import mayavi.mlab as mlab
 import os
 from tqdm import tqdm
 import visualization
+from pc_preprocess import process
 
 
 def generate_frames(pcd_list, output_dir, azimuth=None, elevation=None, distance=None, focalpoint=None):
@@ -20,6 +21,10 @@ def generate_frames(pcd_list, output_dir, azimuth=None, elevation=None, distance
     for pcd_file in tqdm(pcd_list):
         points = o3d.io.read_point_cloud(pcd_file)
         points_np = np.array(points.points)
+
+        # if process is needed
+        points_np = process(points_np)
+
         mlab = visualization.viz_mayavi(points_np, vals="distance", viz=False,
                                         azimuth=azimuth, elevation=elevation, distance=distance, focalpoint=focalpoint)
 
@@ -39,22 +44,34 @@ def generate_one_frame(pcd_file, output_dir, azimuth=None, elevation=None, dista
     mlab.savefig(filename=filename)
 
 
-def batch_generate_frames(pcd_list, output_path, start_frame, end_frame):
+def batch_generate_frames(pcd_list, output_path, start_frame, end_frame, view_dict):
     for i in range(100):
         if start_frame + (i + 1) * 20 < end_frame:
             batch = pcd_list[start_frame + i * 20: start_frame + (i + 1) * 20]
-            generate_frames(batch, output_path, azimuth=180, elevation=80, distance=40, focalpoint=(0, 0, 0))
+            generate_frames(batch, output_path,
+                            azimuth=view_dict['azimuth'],
+                            elevation=view_dict['elevation'],
+                            distance=view_dict['distance'],
+                            focalpoint=view_dict['focal_point'])
 
 
 if __name__ == '__main__':
     dataset_path = '/home/mark/Airport/dataset'
-    output_path = '/home/mark/Airport/frames'
+    output_path = '/home/mark/Airport/frames/projection_view_3'
     pcd_list = sorted(glob.glob(os.path.join(dataset_path, '*.pcd')))
 
     # generate png according to list
-    start_frame = 670
-    # # generate_frames(pcd_list, output_path, azimuth=180, elevation=80, distance=40, focalpoint=(0, 0, 0))
-    batch_generate_frames(pcd_list, output_path, start_frame, len(pcd_list))
+    start_frame = 700
+
+    # initialize view
+    default_view_dict = {'azimuth': 180, 'elevation': 80, 'distance': 40, 'focal_point': (0, 0, 0)}
+    projection_1_view_dict = {'azimuth': 180, 'elevation': 90, 'distance': 40, 'focal_point': (0, 0, 0)}
+    projection_2_view_dict = {'azimuth': 180, 'elevation': 0, 'distance': 150, 'focal_point': (50, 0, 100)}
+    projection_3_view_dict = {'azimuth': 270, 'elevation': 90, 'distance': 200, 'focal_point': (50, 0, 0)}
+
+    # generate frames
+    view_dict = projection_3_view_dict
+    batch_generate_frames(pcd_list, output_path, start_frame, len(pcd_list), view_dict)
 
     # generate png according to a single file
     # idx = 10
